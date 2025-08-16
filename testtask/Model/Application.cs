e49c9251c10f19cf5715e9fec8e13f368c91e2c6
testtask <testtask@RVT-LAB>
2025-08-16 22:06:34 +0300

    Добавьте файлы проекта.

---
+++ testtask/Model/Application.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Ribbon;
+using TestTask.ViewModel;
+
+namespace TestTask.Model
+{
+    [Transaction(TransactionMode.Manual)]
+
+    class Application : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            try {
+                application.CreateRibbonTab("Тестовое задание");
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("Ошибка", ex.Message);
+                return Result.Failed;
+            }
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ testtask/ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TestTask.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _title = "Plugin";
+        private string _message = "test";
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
+        }
+    }
+}
