4f1b264934611591257e3b9068aa0db1ebac8d1e
testtask <testtask@RVT-LAB>
2025-08-17 19:38:29 +0500

    Add icon

--- testtask/Application.cs
+++ testtask/Application.cs
@@ -1,7 +1,10 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System;
+using System.IO;
 using System.Reflection;
+using System.Windows.Media.Imaging;
 
 namespace TestTask
 {
@@ -16,6 +19,9 @@ namespace TestTask
             RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Со смещением");
             var assemblyPath = typeof(Application).Assembly.Location;
             PushButtonData pushButtonData = new PushButtonData("appButton", "Выбор помещения", assemblyPath, "TestTask.Commands.Command");
+            var iconPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assets", "wall32.png");
+            BitmapImage iconImage = new BitmapImage(new Uri(iconPath));
+            pushButtonData.LargeImage = iconImage;
             PushButton pushButton = ribbonPanel.AddItem(pushButtonData) as PushButton;
             return Result.Succeeded;
         }
