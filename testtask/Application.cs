910ca6aa9b06c169ed0324fa244a76bca15e5a0b
testtask <testtask@RVT-LAB>
2025-08-17 13:11:10 +0300

    Refactor Application.cs

---
+++ testtask/Application.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Reflection;
+
+namespace TestTask
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Application : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            var tabName = "Построение стен";
+            application.CreateRibbonTab(tabName);
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel(tabName, "Со смещением");
+            var assemblyPath = typeof(Application).Assembly.Location;
+            PushButtonData pushButtonData = new PushButtonData("appButton", "Выбор помещения", assemblyPath, "TestTask.Model.Command");
+            PushButton pushButton = ribbonPanel.AddItem(pushButtonData) as PushButton;
+            return Result.Succeeded;
+        }
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+    }
+}
--- testtask/Model/Command.cs
+++ testtask/Model/Command.cs
@@ -1,13 +1,35 @@
-﻿using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using System;
+using TestTask.View;
+using TestTask.ViewModel;
 
 namespace TestTask.Model
 {
-    public class Command : IExternalCommand
+    [Transaction(TransactionMode.Manual)]
+
+    class Command : IExternalCommand
     {
+        static AddInId addinid = new AddInId(new Guid("E67C73A9-C790-4EF4-9137-0DC44D0A9F1B"));
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
-            return Result.Succeeded;
+            try
+            {
+                var viewModel = new MainViewModel();
+                var window = new MainWindow
+                {
+                    DataContext = viewModel
+                };
+
+                window.ShowDialog();
+                return Result.Succeeded;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+            }
         }
     }
 }
